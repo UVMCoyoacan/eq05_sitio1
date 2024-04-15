e508bdb75ca9aa9458c3e9a41f39ab75ca16a355
Alfredo <a040091365@my.uvm.edu.mx>
2024-04-14 23:18:12 -0600

    pagina historia

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+    var instagramLink = document.getElementById("instagram-link");
+
+    instagramLink.addEventListener("click", function() {
+        window.location.href = "https://www.instagram.com/servip.service?igsh=dGVqMWc0YmY0MTd0";
+    });
+
+    var whatsappLink = document.getElementById("whatsapp-link");
+
+    whatsappLink.addEventListener("click", function() {
+        window.location.href = "https://wa.me/qr/OHHGUOVS2VO2P1";
+    });
